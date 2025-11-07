b9f4c25f846755cc4cff47916cd989fb5830cdba
Radek Vala <radek.www@gmail.com>
2025-11-07 09:30:10 +0100

    injektuj translator service do translator page a zobraz vysledek prekladu

--- src/app/translator/translator.page.ts
+++ src/app/translator/translator.page.ts
@@ -1,20 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardContent, IonInput, IonButton } from '@ionic/angular/standalone';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { TranslatorService } from '../services/api/translator.service';
 
 @Component({
   selector: 'app-translator',
   templateUrl: './translator.page.html',
   styleUrls: ['./translator.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, ReactiveFormsModule, IonInput, IonCardContent, IonCard, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class TranslatorPage implements OnInit {
 
-  constructor() { }
+  userInput = new FormControl<string>('');
+  result: string | null = null;
+  private translatorService = inject(TranslatorService);
+
+  //constructor(private translatorService: TranslatorService) { }
 
   ngOnInit() {
   }
 
+  async translateClicked() {
+    const input = this.userInput.value
+    console.log('Translating:', input);
+    // Translation logic would go here
+    this.result = await this.translatorService.translate(input || '');
+  }
 }
