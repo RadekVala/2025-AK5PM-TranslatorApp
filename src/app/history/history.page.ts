82f712f858f3dda200ce12127123a141c5c01fb8
Radek Vala <radek.www@gmail.com>
2025-11-07 10:52:35 +0100

    pridej history stránku + store service pro ukládání dat pomocí Preferences

---
+++ src/app/history/history.page.ts
@@ -0,0 +1,30 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardContent } from '@ionic/angular/standalone';
+import { StoreService } from '../services/store.service';
+
+@Component({
+  selector: 'app-history',
+  templateUrl: './history.page.html',
+  styleUrls: ['./history.page.scss'],
+  standalone: true,
+  imports: [IonCardContent, IonCard, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+})
+export class HistoryPage implements OnInit {
+
+  storeService = inject(StoreService)
+
+  constructor() {
+    console.log('History constructor');
+  }
+
+  ngOnInit() {
+    console.log('Angular component History initialized');
+  }
+
+  ionViewWillEnter() {
+    console.log('History ionViewWillEnter - loading history');
+    this.storeService.loadHistory();
+  }
+}
--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Preferences } from '@capacitor/preferences';
+
+export interface TranslationRecord {
+  uuid: string;
+  originalText: string;
+  translatedText: string;
+  timestamp: number;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class StoreService {
-  
+
+  history: TranslationRecord[] = [];
+
+  async store(translationRecord: TranslationRecord): Promise<void> {
+    this.history.unshift(translationRecord);
+    Preferences.set({
+      key: 'history',
+      value: JSON.stringify(this.history),
+    });
+  }
+
+  async loadHistory(): Promise<void> {
+    const { value } = await Preferences.get({ key: 'history' });
+    if (value) {
+      this.history = JSON.parse(value);
+    }
+  }
 }
